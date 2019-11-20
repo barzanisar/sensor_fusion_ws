21528aa4288b1f637aad8a7733a3bd3ed75193ea
barzanisar <barzanisar@sabanciuniv.edu>
2019-11-20 20:20:40 +0500

    first working 3d tracking

--- SFND_3D_Object_Tracking/src/camFusion_Student.cpp
+++ SFND_3D_Object_Tracking/src/camFusion_Student.cpp
@@ -2,21 +2,26 @@
 #include <iostream>
 #include <algorithm>
 #include <numeric>
+
+#include <opencv2/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/xfeatures2d/nonfree.hpp>
 
 #include "camFusion.hpp"
+#include "lidarData.hpp"
 #include "dataStructures.h"
 
 using namespace std;
-
 
 // Create groups of Lidar points whose projection into the camera falls into the same bounding box
 void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<LidarPoint> &lidarPoints, float shrinkFactor, cv::Mat &P_rect_xx, cv::Mat &R_rect_xx, cv::Mat &RT)
 {
     // loop over all Lidar points and associate them to a 2D bounding box
-    cv::Mat X(4, 1, cv::DataType<double>::type);
-    cv::Mat Y(3, 1, cv::DataType<double>::type);
+    cv::Mat X(4, 1, cv::DataType<double>::type); // X,Y,Z,1 points in Lidar frame
+    cv::Mat Y(3, 1, cv::DataType<double>::type); // x,y,z points in camera image
 
     for (auto it1 = lidarPoints.begin(); it1 != lidarPoints.end(); ++it1)
     {
@@ -29,8 +34,8 @@ void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<Li
         // project Lidar point into camera
         Y = P_rect_xx * R_rect_xx * RT * X;
         cv::Point pt;
-        pt.x = Y.at<double>(0, 0) / Y.at<double>(0, 2); // pixel coordinates
-        pt.y = Y.at<double>(1, 0) / Y.at<double>(0, 2);
+        pt.x = Y.at<double>(0, 0) / Y.at<double>(2, 0); // pixel coordinates
+        pt.y = Y.at<double>(1, 0) / Y.at<double>(2, 0);
 
         vector<vector<BoundingBox>::iterator> enclosingBoxes; // pointers to all bounding boxes which enclose the current Lidar point
         for (vector<BoundingBox>::iterator it2 = boundingBoxes.begin(); it2 != boundingBoxes.end(); ++it2)
@@ -52,7 +57,7 @@ void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<Li
 
         // check wether point has been enclosed by one or by multiple boxes
         if (enclosingBoxes.size() == 1)
-        { 
+        {
             // add Lidar point to bounding box
             enclosingBoxes[0]->lidarPoints.push_back(*it1);
         }
@@ -60,53 +65,52 @@ void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<Li
     } // eof loop over all Lidar points
 }
 
-
 void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, cv::Size imageSize, bool bWait)
 {
     // create topview image
     cv::Mat topviewImg(imageSize, CV_8UC3, cv::Scalar(255, 255, 255));
 
-    for(auto it1=boundingBoxes.begin(); it1!=boundingBoxes.end(); ++it1)
+    for (auto it1 = boundingBoxes.begin(); it1 != boundingBoxes.end(); ++it1)
     {
         // create randomized color for current 3D object
         cv::RNG rng(it1->boxID);
-        cv::Scalar currColor = cv::Scalar(rng.uniform(0,150), rng.uniform(0, 150), rng.uniform(0, 150));
+        cv::Scalar currColor = cv::Scalar(rng.uniform(0, 150), rng.uniform(0, 150), rng.uniform(0, 150));
 
         // plot Lidar points into top view image
-        int top=1e8, left=1e8, bottom=0.0, right=0.0; 
-        float xwmin=1e8, ywmin=1e8, ywmax=-1e8;
+        int top = 1e8, left = 1e8, bottom = 0.0, right = 0.0;
+        float xwmin = 1e8, ywmin = 1e8, ywmax = -1e8;
         for (auto it2 = it1->lidarPoints.begin(); it2 != it1->lidarPoints.end(); ++it2)
         {
             // world coordinates
             float xw = (*it2).x; // world position in m with x facing forward from sensor
             float yw = (*it2).y; // world position in m with y facing left from sensor
-            xwmin = xwmin<xw ? xwmin : xw;
-            ywmin = ywmin<yw ? ywmin : yw;
-            ywmax = ywmax>yw ? ywmax : yw;
+            xwmin = xwmin < xw ? xwmin : xw;
+            ywmin = ywmin < yw ? ywmin : yw;
+            ywmax = ywmax > yw ? ywmax : yw;
 
             // top-view coordinates
             int y = (-xw * imageSize.height / worldSize.height) + imageSize.height;
             int x = (-yw * imageSize.width / worldSize.width) + imageSize.width / 2;
 
             // find enclosing rectangle
-            top = top<y ? top : y;
-            left = left<x ? left : x;
-            bottom = bottom>y ? bottom : y;
-            right = right>x ? right : x;
+            top = top < y ? top : y;
+            left = left < x ? left : x;
+            bottom = bottom > y ? bottom : y;
+            right = right > x ? right : x;
 
             // draw individual point
             cv::circle(topviewImg, cv::Point(x, y), 4, currColor, -1);
         }
 
         // draw enclosing rectangle
-        cv::rectangle(topviewImg, cv::Point(left, top), cv::Point(right, bottom),cv::Scalar(0,0,0), 2);
+        cv::rectangle(topviewImg, cv::Point(left, top), cv::Point(right, bottom), cv::Scalar(0, 0, 0), 2);
 
         // augment object with some key data
         char str1[200], str2[200];
         sprintf(str1, "id=%d, #pts=%d", it1->boxID, (int)it1->lidarPoints.size());
-        putText(topviewImg, str1, cv::Point2f(left-250, bottom+50), cv::FONT_ITALIC, 2, currColor);
-        sprintf(str2, "xmin=%2.2f m, yw=%2.2f m", xwmin, ywmax-ywmin);
-        putText(topviewImg, str2, cv::Point2f(left-250, bottom+125), cv::FONT_ITALIC, 2, currColor);  
+        putText(topviewImg, str1, cv::Point2f(left, bottom + 20), cv::FONT_ITALIC, 0.5, currColor);
+        sprintf(str2, "xmin=%2.2f m, yw=%2.2f m", xwmin, ywmax - ywmin);
+        putText(topviewImg, str2, cv::Point2f(left, bottom + 50), cv::FONT_ITALIC, 0.5, currColor);
     }
 
     // plot distance markers
@@ -123,36 +127,237 @@ void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, 
     cv::namedWindow(windowName, 1);
     cv::imshow(windowName, topviewImg);
 
-    if(bWait)
+    if (bWait)
     {
         cv::waitKey(0); // wait for key to be pressed
     }
 }
 
-
 // associate a given bounding box with the keypoints it contains
 void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, std::vector<cv::DMatch> &kptMatches)
 {
-    // ...
-}
+    std::vector<cv::DMatch> potentialMatchesInBB;
+    std::vector<float> distanceMoved;
+    float meanDist = 0;
+    for (auto match : kptMatches)
+    {
+        if (boundingBox.roi.contains(kptsCurr[match.trainIdx].pt))
+        {
+            float dist = cv::norm(kptsCurr[match.trainIdx].pt - kptsPrev[match.queryIdx].pt);
+            distanceMoved.push_back(dist);
+            meanDist += dist;
 
+            potentialMatchesInBB.push_back(match);
+        }
+    }
+
+    //float mean = std::accumulate(distanceMoved.begin(), distanceMoved.end(), 0.0) / distanceMoved.size();
+    meanDist = meanDist / distanceMoved.size();
+    cout << "mean dist moved by keypts " << meanDist << endl;
+
+    float distanceThreshold = 1.5 * meanDist; //pixels
+
+    for (uint i = 0; i < potentialMatchesInBB.size(); ++i)
+    {
+        if (distanceMoved[i] < distanceThreshold)
+        {
+            boundingBox.kptMatches.push_back(potentialMatchesInBB[i]);
+        }
+    }
+}
 
 // Compute time-to-collision (TTC) based on keypoint correspondences in successive images
-void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, 
+void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr,
                       std::vector<cv::DMatch> kptMatches, double frameRate, double &TTC, cv::Mat *visImg)
 {
-    // ...
+    // compute distance ratios between all matched keypoints
+    vector<double> distRatios; // stores the distance ratios for all keypoints between curr. and prev. frame
+    for (auto it1 = kptMatches.begin(); it1 != kptMatches.end() - 1; ++it1)
+    { // outer kpt. loop
+
+        // get current keypoint and its matched partner in the prev. frame
+        cv::KeyPoint kpOuterCurr = kptsCurr.at(it1->trainIdx);
+        cv::KeyPoint kpOuterPrev = kptsPrev.at(it1->queryIdx);
+
+        for (auto it2 = kptMatches.begin() + 1; it2 != kptMatches.end(); ++it2)
+        { // inner kpt.-loop
+
+            double minDist = 100.0; // min. required distance
+
+            // get next keypoint and its matched partner in the prev. frame
+            cv::KeyPoint kpInnerCurr = kptsCurr.at(it2->trainIdx);
+            cv::KeyPoint kpInnerPrev = kptsPrev.at(it2->queryIdx);
+
+            // compute distances and distance ratios
+            double distCurr = cv::norm(kpOuterCurr.pt - kpInnerCurr.pt);
+            double distPrev = cv::norm(kpOuterPrev.pt - kpInnerPrev.pt);
+
+            if (distPrev > std::numeric_limits<double>::epsilon() && distCurr >= minDist)
+            { // avoid division by zero
+
+                double distRatio = distCurr / distPrev;
+                distRatios.push_back(distRatio);
+            }
+        } // eof inner loop over all matched kpts
+    }     // eof outer loop over all matched kpts
+
+    // only continue if list of distance ratios is not empty
+    if (distRatios.size() == 0)
+    {
+        TTC = NAN;
+        return;
+    }
+
+    std::sort(distRatios.begin(), distRatios.end());
+    long medIndex = floor(distRatios.size() / 2.0);
+    double medDistRatio = distRatios.size() % 2 == 0 ? (distRatios[medIndex - 1] + distRatios[medIndex]) / 2.0 : distRatios[medIndex]; // compute median dist. ratio to remove outlier influence
+
+    float dT = 1 / frameRate;
+    TTC = -dT / (1 - medDistRatio);
 }
 
+float findRobustMinX(std::vector<LidarPoint> &lidarPoints)
+{
+    sort(lidarPoints.begin(), lidarPoints.end(), [](const LidarPoint &lhs, const LidarPoint &rhs) {
+        return lhs.x < rhs.x;
+    });
 
+    //return lidarPoints[0].x;
+
+    unsigned int firstNpts = lidarPoints.size() / 2; // 100
+
+    //return lidarPoints[firstNpts/2].x; //median of first n pts
+
+    float mean = std::accumulate(lidarPoints.begin(), lidarPoints.begin() + firstNpts, 0.0, [](double sum, const LidarPoint &lpt) { return sum + lpt.x; }) / firstNpts;
+
+    //return mean;
+    
+    std::vector<float> diff(firstNpts);
+    std::transform(lidarPoints.begin(), lidarPoints.begin() + firstNpts, diff.begin(), [mean](const LidarPoint &lpt) { return lpt.x - mean; });
+
+    double sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+    float stdev = std::sqrt(sq_sum / firstNpts);
+
+    float lowerBound = mean - 2 * stdev;
+
+    cout << "mean " << mean << endl;
+    cout << "lowerBound " << lowerBound << endl;
+
+    for (auto lpt : lidarPoints)
+    {
+        if (lpt.x > lowerBound) // check if lpt.x is inlier
+        {
+            return lpt.x;
+        }
+        else
+        {
+            cout << "outlier " << lpt.x << endl;
+        }
+    }
+}
 void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
                      std::vector<LidarPoint> &lidarPointsCurr, double frameRate, double &TTC)
 {
-    // ...
+    float minXPrev = findRobustMinX(lidarPointsPrev);
+    cout << endl << "minXPrev " << minXPrev << endl;
+    showLidarTopview(lidarPointsPrev, cv::Size(5.5, 10.0), cv::Size(400, 800), minXPrev, true);
+    float minXCurr = findRobustMinX(lidarPointsCurr);
+    cout << "minXCurr " << minXCurr << endl;
+    showLidarTopview(lidarPointsCurr, cv::Size(5.5, 10.0), cv::Size(400, 800) ,minXCurr, true);
+
+    float velocity = (minXPrev - minXCurr) * frameRate;
+
+    TTC = minXCurr / velocity;
 }
 
-
-void matchBoundingBoxes(std::vector<cv::DMatch> &matches, std::map<int, int> &bbBestMatches, DataFrame &prevFrame, DataFrame &currFrame)
+void matchBoundingBoxes(const std::vector<cv::DMatch> &matches, std::map<int, int> &bbBestMatches, const DataFrame &prevFrame, const DataFrame &currFrame)
 {
-    // ...
+    if (prevFrame.boundingBoxes.size() == 0 || currFrame.boundingBoxes.size() == 0)
+    {
+        std::cout << "ERROR: No bounding boxes found! " << std::endl;
+        return;
+    }
+
+    std::cout << "prevFrame.boundingBoxes.size() " << prevFrame.boundingBoxes.size() << "currFrame.boundingBoxes.size() " << currFrame.boundingBoxes.size() << std::endl;
+    cv::Mat hist = cv::Mat::zeros(prevFrame.boundingBoxes.size(), currFrame.boundingBoxes.size(), CV_16UC1); //cv::DataType<uint>::type
+
+    std::vector<cv::DMatch> selMatches;
+    for (auto match : matches)
+    {
+        std::vector<int> prevFrameBBoxIds, currFrameBBoxIds;
+        //int prevFrameBBoxId, currFrameBBoxId;
+        cv::Point2f prevFramePt = prevFrame.keypoints[match.queryIdx].pt;
+        cv::Point2f currFramePt = currFrame.keypoints[match.trainIdx].pt;
+
+        for (auto bBox : prevFrame.boundingBoxes)
+        {
+            if (bBox.roi.contains(prevFramePt))
+            {
+                prevFrameBBoxIds.push_back(bBox.boxID);
+            }
+        }
+
+        for (auto bBox : currFrame.boundingBoxes)
+        {
+            if (bBox.roi.contains(currFramePt))
+            {
+                currFrameBBoxIds.push_back(bBox.boxID);
+            }
+        }
+
+        if (prevFrameBBoxIds.size() == 1 && currFrameBBoxIds.size() == 1)
+        {
+            hist.at<uint16_t>(prevFrameBBoxIds[0], currFrameBBoxIds[0]) += 1;
+            // if (prevFrameBBoxIds[0] == 5 || currFrameBBoxIds[0] == 5)
+            // {
+            //     selMatches.push_back(match);
+            //     std::cout <<  "prevFrameBBoxIds[0] " << prevFrameBBoxIds[0] << " " <<  currFrameBBoxIds[0] << std::endl;
+            //     std::cout << "hist(5,5) " << hist.at<uint16_t>(prevFrameBBoxIds[0], currFrameBBoxIds[0]) << std::endl;
+            // }
+        }
+    }
+
+    // //print hist for debugging
+    // for (uint row = 0; row < hist.rows; ++row){
+    //     for (uint col = 0; col < hist.cols; ++col)
+    //     {
+    //         cout << hist.at<uint16_t>(row,col) << ", ";
+    //     }
+    //     cout << endl;
+    // }
+
+    for (uint i = 0; i < hist.rows; i++)
+    {
+        double max = 0;
+        cv::Point maxLoc;
+        cv::minMaxLoc(hist.row(i), NULL, &max, NULL, &maxLoc);
+
+        uint minNumberOfMatches = 10;
+        std::cout << "row " << i << " col " << maxLoc.x << " val " << hist.at<uint16_t>(i, maxLoc.x) << std::endl;
+
+        if (hist.at<uint16_t>(i, maxLoc.x) > minNumberOfMatches)
+        {
+            std::cout << "prevFrame bbox " << i << " matches with currFrame bbox " << maxLoc.x << std::endl;
+            bbBestMatches.emplace(i, maxLoc.x);
+        }
+    }
+
+    // // visualize matches between current and previous image
+    // bool bVis = true;
+    // if (bVis)
+    // {
+    //     cv::Mat matchImg = (currFrame.cameraImg).clone();
+    //     cv::drawMatches(prevFrame.cameraImg, prevFrame.keypoints,
+    //                     currFrame.cameraImg, currFrame.keypoints,
+    //                     selMatches, matchImg,
+    //                     cv::Scalar::all(-1), cv::Scalar::all(-1),
+    //                     vector<char>(), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+    //     string windowName = "Matching keypoints between two camera images";
+    //     cv::namedWindow(windowName, 7);
+    //     cv::imshow(windowName, matchImg);
+    //     cout << "Press key to continue to next image" << endl;
+    //     cv::waitKey(0); // wait for key to be pressed
+    // }
+    // bVis = false;
 }
--- SFND_3D_Object_Tracking/src/matching2D_Student.cpp
+++ SFND_3D_Object_Tracking/src/matching2D_Student.cpp
@@ -1,4 +1,3 @@
-
 #include <numeric>
 #include "matching2D.hpp"
 
@@ -9,38 +8,85 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
                       std::vector<cv::DMatch> &matches, std::string descriptorType, std::string matcherType, std::string selectorType)
 {
     // configure matcher
-    bool crossCheck = false;
+    bool crossCheck = selectorType.compare("SEL_KNN") == 0 ? false : true; // either use 1nn with crossCheck = true OR knn with distance ratio test and crossCheck=false
     cv::Ptr<cv::DescriptorMatcher> matcher;
 
     if (matcherType.compare("MAT_BF") == 0)
     {
-        int normType = cv::NORM_HAMMING;
+        int normType = descriptorType.compare("DES_BINARY") == 0 ? cv::NORM_HAMMING : cv::NORM_L2;
         matcher = cv::BFMatcher::create(normType, crossCheck);
     }
     else if (matcherType.compare("MAT_FLANN") == 0)
     {
-        // ...
+        if (descSource.type() != CV_32F)
+        { // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
+            descSource.convertTo(descSource, CV_32F);
+            descRef.convertTo(descRef, CV_32F);
+        }
+
+        matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
+        cout << "FLANN matching";
     }
 
     // perform matching task
     if (selectorType.compare("SEL_NN") == 0)
     { // nearest neighbor (best match)
 
+        double t = (double)cv::getTickCount();
         matcher->match(descSource, descRef, matches); // Finds the best match for each descriptor in desc1
+        t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        cout << " Nearest Neighbor with n=" << matches.size() << " matches in " << 1000 * t / 1.0 << " ms" << endl;
     }
     else if (selectorType.compare("SEL_KNN") == 0)
     { // k nearest neighbors (k=2)
 
-        // ...
+        vector<vector<cv::DMatch>> knn_matches;
+        double t = (double)cv::getTickCount();
+        matcher->knnMatch(descSource, descRef, knn_matches, 2); // finds the 2 best matches
+        t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        cout << " (KNN) with " << knn_matches.size() << " matches in " << 1000 * t / 1.0 << " ms" << endl;
+
+        // filter matches using descriptor distance ratio test
+        double minDescDistRatio = 0.8;
+        for (auto it = knn_matches.begin(); it != knn_matches.end(); ++it)
+        {
+
+            if ((*it)[0].distance < minDescDistRatio * (*it)[1].distance)
+            {
+                matches.push_back((*it)[0]);
+            }
+        }
+        cout << "# keypoints removed = " << knn_matches.size() - matches.size() << endl;
+    }
+
+    cout << matcherType << " with " << selectorType << " found " << matches.size() << " matches" << endl;
+
+    if (descSource.type() != CV_8U)
+    {
+        descSource.convertTo(descSource, CV_8U);
+        descRef.convertTo(descRef, CV_8U);
     }
 }
 
 // Use one of several types of state-of-art descriptors to uniquely identify keypoints
 void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descriptors, string descriptorType)
 {
+    // BRIEF, ORB, FREAK, AKAZE, SIFT
     // select appropriate descriptor
     cv::Ptr<cv::DescriptorExtractor> extractor;
-    if (descriptorType.compare("BRISK") == 0)
+    if (descriptorType.compare("SIFT") == 0)
+    {
+        extractor = cv::xfeatures2d::SiftDescriptorExtractor::create();
+    }
+    else if (descriptorType.compare("BRIEF") == 0)
+    {
+        extractor = cv::xfeatures2d::BriefDescriptorExtractor::create();
+    }
+    else if (descriptorType.compare("ORB") == 0)
+    {
+        extractor = cv::ORB::create();
+    }
+    else if (descriptorType.compare("BRISK") == 0)
     {
 
         int threshold = 30;        // FAST/AGAST detection threshold score.
@@ -49,10 +95,14 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
 
         extractor = cv::BRISK::create(threshold, octaves, patternScale);
     }
-    else
+    else if (descriptorType.compare("FREAK") == 0)
     {
-
-        //...
+        extractor = cv::xfeatures2d::FREAK::create();
+    }
+    else if (descriptorType.compare("AKAZE") == 0)
+    {
+        // AKAZE uses binary descriptor by default
+        extractor = cv::AKAZE::create();
     }
 
     // perform feature description
@@ -60,6 +110,76 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
     extractor->compute(img, keypoints, descriptors);
     t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
     cout << descriptorType << " descriptor extraction in " << 1000 * t / 1.0 << " ms" << endl;
+}
+
+void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis)
+{
+
+    // Detector parameters
+    int blockSize = 2;     // for every pixel, a blockSize × blockSize neighborhood is considered
+    int apertureSize = 3;  // aperture parameter for Sobel operator (must be odd)
+    int minResponse = 100; // minimum value for a corner in the 8bit scaled response matrix
+    double k = 0.04;       // Harris parameter (see equation for details)
+
+    // Detect Harris corners and normalize output
+    double t = (double)cv::getTickCount();
+    cv::Mat dst, dst_norm, dst_norm_scaled;
+    dst = cv::Mat::zeros(img.size(), CV_32FC1);
+    cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
+    cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+
+    // Look for prominent corners and instantiate keypoints
+    double maxOverlap = 0.0; // max. permissible overlap between two features in %, used during non-maxima suppression
+    for (size_t j = 0; j < dst_norm.rows; j++)
+    {
+        for (size_t i = 0; i < dst_norm.cols; i++)
+        {
+            int response = (int)dst_norm.at<float>(j, i);
+            if (response > minResponse)
+            { // only store points above a threshold
+
+                cv::KeyPoint newKeyPoint;
+                newKeyPoint.pt = cv::Point2f(i, j);
+                newKeyPoint.size = 2 * apertureSize;
+                newKeyPoint.response = response;
+
+                // perform non-maximum suppression (NMS) in local neighbourhood around new key point
+                bool bOverlap = false;
+                for (auto it = keypoints.begin(); it != keypoints.end(); ++it)
+                {
+                    double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
+                    if (kptOverlap > maxOverlap)
+                    {
+                        bOverlap = true;
+                        if (newKeyPoint.response > (*it).response)
+                        {                      // if overlap is >t AND response is higher for new kpt
+                            *it = newKeyPoint; // replace old key point with new one
+                            break;             // quit loop over keypoints
+                        }
+                    }
+                }
+                if (!bOverlap)
+                {                                     // only add new key point if no overlap has been found in previous NMS
+                    keypoints.push_back(newKeyPoint); // store new keypoint in dynamic list
+                }
+            }
+        } // eof loop over cols
+    }     // eof loop over rows
+
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "Harris detection with n=" << keypoints.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+
+    // visualize keypoints
+    if (bVis)
+    {
+        cv::Mat visImage = dst_norm_scaled.clone();
+        cv::drawKeypoints(dst_norm_scaled, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        string windowName = "Harris Corner Detector Results";
+        cv::namedWindow(windowName);
+        imshow(windowName, visImage);
+        cv::waitKey(0);
+    }
 }
 
 // Detect keypoints in image using the traditional Shi-Thomasi detector
@@ -71,18 +191,18 @@ void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool b
     double minDistance = (1.0 - maxOverlap) * blockSize;
     int maxCorners = img.rows * img.cols / max(1.0, minDistance); // max. num. of keypoints
 
-    double qualityLevel = 0.01; // minimal accepted quality of image corners
+    double qualityLevel = 0.01; // minimal accepted quality of image corners (choose value from 0-1)
     double k = 0.04;
+    bool useHarris = false;
 
     // Apply corner detection
     double t = (double)cv::getTickCount();
     vector<cv::Point2f> corners;
-    cv::goodFeaturesToTrack(img, corners, maxCorners, qualityLevel, minDistance, cv::Mat(), blockSize, false, k);
+    cv::goodFeaturesToTrack(img, corners, maxCorners, qualityLevel, minDistance, cv::Mat(), blockSize, useHarris, k);
 
     // add corners to result vector
     for (auto it = corners.begin(); it != corners.end(); ++it)
     {
-
         cv::KeyPoint newKeyPoint;
         newKeyPoint.pt = cv::Point2f((*it).x, (*it).y);
         newKeyPoint.size = blockSize;
@@ -97,6 +217,61 @@ void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool b
         cv::Mat visImage = img.clone();
         cv::drawKeypoints(img, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
         string windowName = "Shi-Tomasi Corner Detector Results";
+        cv::namedWindow(windowName, 6);
+        imshow(windowName, visImage);
+        cv::waitKey(0);
+    }
+}
+
+void detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis)
+{
+    // FAST, BRISK, ORB, AKAZE, SIFT
+    cv::Ptr<cv::FeatureDetector> detector;
+
+    // SIFT allows detection of large and small keypoints i.e Keypoints of different scale.
+    // Blob Detection:
+    // Difference of Gaussians are computed across the image at different scales.
+    // Corners are the extrema of DoG across scale and space (local neighborhood).
+    // The scale that gives highest value of DoG is recorded with the x,y location of the keypoint.
+    // DoG are high for edges so edges are rejected by Harris corner detection method.
+    // One eigen value of Harris Hessian matrix is higher than the other for edges.
+    // But for corners both are high.
+    // Descriptor: Each keypoint (x,y,scale) is described by a 128 floats vector containing HOG over the neighborhood of the keypoint.
+    if (detectorType.compare("SIFT") == 0)
+    {
+        detector = cv::xfeatures2d::SIFT::create();
+    }
+    else if (detectorType.compare("FAST") == 0)
+    {
+        int threshold = 30;                                                              // difference between intensity of the central pixel and pixels of a circle around this pixel
+        bool bNMS = true;                                                                // perform non-maxima suppression on keypoints
+        cv::FastFeatureDetector::DetectorType type = cv::FastFeatureDetector::TYPE_9_16; // TYPE_9_16, TYPE_7_12, TYPE_5_8
+        detector = cv::FastFeatureDetector::create(threshold, bNMS, type);
+    }
+    else if (detectorType.compare("ORB") == 0)
+    {
+        detector = cv::ORB::create();
+    }
+    else if (detectorType.compare("BRISK") == 0)
+    {
+        detector = cv::BRISK::create();
+    }
+    else if (detectorType.compare("AKAZE") == 0)
+    {
+        detector = cv::AKAZE::create();
+    }
+
+    double t = (double)cv::getTickCount();
+    detector->detect(img, keypoints);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << detectorType << " with n= " << keypoints.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+
+    // visualize results
+    if (bVis)
+    {
+        cv::Mat visImage = img.clone();
+        cv::drawKeypoints(img, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        string windowName = detectorType + " Corner Detector Results";
         cv::namedWindow(windowName, 6);
         imshow(windowName, visImage);
         cv::waitKey(0);
